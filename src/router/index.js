d8bb89b8c13ef7d60be4bfa32dd7e5807333d04c
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-02-16 14:58:58 +0100

    Added menu view and mock api

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Menu from "../views/Menu.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
+   {
+      path: "/",
+      name: "Home",
+      component: Home,
+   },
+   {
+      path: "/about",
+      name: "About",
+      component: About,
+   },
+   {
+      path: "/menu",
+      name: "Menu",
+      component: Menu,
+   },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+   mode: "history",
+   base: process.env.BASE_URL,
+   routes,
+});
 
-export default router
+export default router;
